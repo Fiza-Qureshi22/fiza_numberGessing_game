16d8351336dae9cc2aeb1bbd0376b2db2a783562
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-10 22:40:08 +0500

    no changes
    "Added a CLI-based number guessing game with interactive feedback."

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+//CLI-NUMBER-GUESSING-GAME
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+
+const answer = await inquirer.prompt([
+
+{
+    name:"userGuessNumber",
+    message:"plz guessed a number between 1-6",
+    type:"number",
+},
+])
+if(answer.userGuessNumber === randomNumber)
+{
+   console.log("congratulations you Guessed right number")
+}
+
+else{
+    console.log("you Guess wrong number")
+}
